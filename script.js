9e10b354c116783e0916637f3e1fafaab4bacb93
Jinx <chisel.delahanty@gmail.com>
2019-12-12 20:43:47 +0100

    Create first draft of 4 basic calculator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const calculator = {
+	add: function(x, y) {
+		return x + y;
+	},
+	subtract: function(x, y) {
+		return x - y;
+	},
+	multiply: function(x, y) {
+		return x * y;
+	},
+	divide: function(x, y) {
+		return x / y;
+	},
+};
+
+console.log(calculator.add(1, 2));
+console.log(calculator.subtract(1, 2));
+console.log(calculator.multiply(1, 2));
+console.log(calculator.divide(1, 2));
