eba85ea3a8808931645367e8331c9a901041d3a2
Jinx <chisel.delahanty@gmail.com>
2019-12-15 17:15:23 -0500

    Add event listeners and display button text on screen

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+// const btn1 = document.getElementById("one");
+// const btn2 = document.getElementById("two");
+// const btn3 = document.getElementById("three");
+// const btn4 = document.getElementById("four");
+// const btn5 = document.getElementById("five");
+// const btn6 = document.getElementById("six");
+// const btn7 = document.getElementById("seven");
+// const btn8 = document.getElementById("eight");
+// const btn9 = document.getElementById("nine");
+// const btn0 = document.getElementById("zero");
+// const btnAdd = document.getElementById("add");
+// const btnSubtract = document.getElementById("subtract");
+// const btnMultiply = document.getElementById("multiply");
+// const btnDivide = document.getElementById("divide");
+// const btnEquals = document.getElementById("equals");
+// const btnClear = document.getElementById("clear");
+
+const buttons = document.querySelectorAll(".button");
+const screen = document.getElementById("screen");
+
+let buttonClicked;
+let currentNumber = "";
+let currentOperation = [];
+let displayValue = "";
+
+buttons.forEach(button => {
+	button.addEventListener("click", e => {
+		buttonClicked = button;
+		console.log(buttonClicked);
+
+		// Handle clicks on number buttons
+		if (buttonClicked.classList.contains("number")) {
+			currentNumber += buttonClicked.textContent;
+			displayValue = currentNumber;
+			screen.textContent = displayValue;
+
+		// Handle clicks on non-number buttons
+		} else {
+			if (currentNumber) {
+				currentOperation.push(currentNumber);
+				currentNumber = "";
+				displayValue = buttonClicked.textContent;
+				screen.textContent = displayValue;
+			}
+		}
+	});
+});
+
+
 const calculator = {
 	add: function(x, y) {
 		return x + y;
@@ -12,11 +61,14 @@ const calculator = {
 		return x / y;
 	},
 	operate: function(operator, x, y) {
-		return this.operator(x, y);
+		return operator(x, y);
 	}
 };
 
-console.log(calculator.add(1, 2));
-console.log(calculator.subtract(1, 2));
-console.log(calculator.multiply(1, 2));
-console.log(calculator.divide(1, 2));
+
+// console.log(calculator.operate(calculator.subtract, 1, 2));
+
+
+
+
+////////
